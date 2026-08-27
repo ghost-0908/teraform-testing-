689e0c8c4f978484a5d4756b6224808f82f84bce
vivekhs-09 <vivekhs@nousinfo.com>
2026-08-27 19:40:47 +0530

    Use available Azure VM size in East Asia

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "azurerm_linux_virtual_machine" "this" {
   name                            = "arti-vm"
   location                        = var.location
   resource_group_name             = azurerm_resource_group.this.name
-  size                            = "Standard_B1s"
+  size                            = "Standard_B2ats_v2"
   admin_username                  = "azureadmin"
   admin_password                  = var.admin_password
   disable_password_authentication = false
